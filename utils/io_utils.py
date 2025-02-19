d312707dec9255652750e2ea02cf81a63157e679
axiomcura <erikishere3@gmail.com>
2025-02-19 12:45:21 -0700

    Enhance load_config function to support JSON files and improve error handling

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -2,34 +2,51 @@
 
 This module will contain functions pertaining to loading and writing files.
 """
+import json
 import pathlib
 
 import yaml
 
 
-def load_config(fpath: str | pathlib.Path ) -> dict:
-    """Loads in configuration file if specified path
+def load_config(fpath: str | pathlib.Path) -> dict:
+    """ Loads a configuration file from the specified path.
 
     Parameters
     ----------
     fpath : str | pathlib.Path
-        path pointing to config file
+        Path pointing to the configuration file. It can be a string or a pathlib.Path
+        object.
 
     Returns
     -------
     dict
-        contents of the configuration file
+        Contents of the configuration file as a dictionary.
+
+    Raises
+    ------
+    TypeError
+        If 'fpath' is not a string or pathlib.Path object.
+    ValueError
+        If the file extension is not .json or .yaml.
+    FileNotFoundError
+        If the file does not exist.
     """
 
-    # type checking
-    if not isinstance(fpath, (str | pathlib.Path)):
+    # Type checking
+    if not isinstance(fpath, (str, pathlib.Path)):
         raise TypeError("'fpath' must be a string or pathlib.Path object")
     if isinstance(fpath, str):
         fpath = pathlib.Path(fpath)
 
-    # check if the path exists and returns the full path
+    # Resolve the path and check if it exists
     fpath = fpath.resolve(strict=True)
 
-    # next is to load the yaml file
-    with open(fpath) as content:
-        return yaml.safe_load(content)
+    # Load the configuration file based on its extension
+    if fpath.suffix == ".json":
+        with open(fpath) as content:
+            return json.load(content)
+    elif fpath.suffix == ".yaml":
+        with open(fpath) as content:
+            return yaml.safe_load(content)
+    else:
+        raise ValueError("Only .json and .yaml files are supported")
